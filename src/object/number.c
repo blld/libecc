dc846e8f20a26d1d9f5d61a56824e190c1a9c2de
Aurélien Bouilland <aurelien@bouilland.net>
2015-10-31 16:44:03 +0900

    add toExponential

--- src/object/number.c
+++ src/object/number.c
@@ -13,6 +13,31 @@
 struct Object * Number(prototype) = NULL;
 struct Function * Number(constructor) = NULL;
 
+static struct Value toExponential (const struct Op ** const ops, struct Ecc * const ecc)
+{
+	struct Value value;
+	
+	Op.assertParameterCount(ecc, 1);
+	
+	if (ecc->this.type != Value(number))
+		Ecc.jmpEnv(ecc, Value.error(Error.typeError((*ops)->text, "is not an number")));
+	
+	value = Op.argument(ecc, 0);
+	if (value.type != Value(undefined))
+	{
+		int32_t precision = Value.toInteger(value).data.integer;
+		
+		if (precision < 0 || precision > 48)
+			Ecc.jmpEnv(ecc, Value.error(Error.rangeError((*ops)->text, "precision %d out of range", precision)));
+		
+		ecc->result = Value.chars(Chars.create("%.*e", precision, ecc->this.data.number->value));
+	}
+	else
+		ecc->result = Value.chars(Chars.create("%e", ecc->this.data.number->value));
+	
+	return Value.undefined();
+}
+
 static struct Value toString (const struct Op ** const ops, struct Ecc * const ecc)
 {
 	struct Value value;
@@ -75,6 +100,7 @@ void setup ()
 	Number(prototype) = Object.create(Object(prototype));
 	Function.addToObject(Number(prototype), "toString", toString, 1, flags);
 	Function.addToObject(Number(prototype), "valueOf", valueOf, 0, flags);
+	Function.addToObject(Number(prototype), "toExponential", toExponential, 1, flags);
 	
 	Number(constructor) = Function.createWithNative(NULL, constructorFunction, 1);
 	Object.add(&Number(constructor)->object, Key.makeWithCString("MAX_VALUE"), Value.binary(DBL_MAX), flags);
