20ad0719d63036993f38ac453b009a40f8631898
Aurélien Bouilland <aurelien@bouilland.net>
2015-11-01 16:53:41 +0900

    add math tests

--- src/main.c
+++ src/main.c
@@ -554,6 +554,14 @@ static void testNumber (void)
 	test("-2147483647..toString(16)", "NaN");
 	test("-2147483647..toString(10)", "-2147483647");
 	test("-2147483647 .toString()", "-2147483647");
+	
+	test("123.456.toExponential()", "1.234560e+02");
+	test("123.456.toFixed()", "123.456000");
+	test("123.456.toPrecision()", "123.456");
+	
+	test("123.456.toExponential(10)", "1.2345600000e+02");
+	test("123.456.toFixed(10)", "123.4560000000");
+	test("123.456.toPrecision(10)", "123.456");
 }
 
 static int runTest (int verbosity)
